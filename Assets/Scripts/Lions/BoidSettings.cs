3c3f87d514ad469f24b3d4176dbc27918a9cc687
boomo <randomerz56@gmail.com>
2021-07-10 15:44:08 -0400

    lion seekers

--- Assets/Scripts/Lions/BoidSettings.cs
+++ Assets/Scripts/Lions/BoidSettings.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class BoidSettings : MonoBehaviour
 {
     public float radius = 1;
+
     public float separationWeight = 1;
     public float alignmentWeight = 1;
-    public float centerWeight = 1;
-
+    public float centerWeight = 1; // center does not work
     public float boundaryWeight = 1;
-    public float targetWeight = 1;
+    public float avoidTargetWeight = 1;
 
     public float speed = 1;
     public float rotationSpeed = 10;
+
+
+    public float distUntilAttack = 4;
+    public float acceleration = 1;
 }
--- Assets/Scripts/Lions/Lion1.cs
+++ Assets/Scripts/Lions/Lion1.cs
@@ -9,6 +9,12 @@ public class Lion1 : MonoBehaviour
     public BoidSettings boidSettings;
     public Vector3 direction = Vector3.zero;
 
+    // for gizmos
+    private Vector3 boundaryVector = Vector3.zero;
+    private Vector3 avoidTargetVector = Vector3.zero;
+    private Vector3 newDirection = Vector3.zero;
+
+    public SpriteRenderer spriteRenderer;
 
     enum MoveState
     {
@@ -16,38 +22,24 @@ public class Lion1 : MonoBehaviour
         Vibing,
     }
 
-    void Start()
+    public void Start()
     {
+        moveState = MoveState.Vibing;
         direction = Random.insideUnitCircle;
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, GetAngle(direction)));
     }
     
     void Update()
     {
-        UpdateDirection();
+        if (moveState == MoveState.Vibing)
+        {
+            UpdateDirectionVibing();
 
-        transform.position += transform.up * boidSettings.speed * Time.deltaTime;
-
-        if (transform.position.x > 8)
-        {
-            transform.position = new Vector3(-8, transform.position.y);
-        }
-        else if (transform.position.x < -8)
-        {
-            transform.position = new Vector3(8, transform.position.y);
-        }
-
-        if (transform.position.y > 4.5f)
-        {
-            transform.position = new Vector3(transform.position.x, -4.5f);
-        }
-        else if (transform.position.y < -4.5f)
-        {
-            transform.position = new Vector3(transform.position.x, 4.5f);
+            transform.position += transform.up * boidSettings.speed * Time.deltaTime;
         }
     }
 
-    private void UpdateDirection()
+    private void UpdateDirectionVibing()
     {
         Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, boidSettings.radius, LayerMask.GetMask("Lion")); // no view cone
 
@@ -65,34 +57,140 @@ public class Lion1 : MonoBehaviour
                     continue;
                 separationVector += dif * (boidSettings.radius / dif.sqrMagnitude);
                 alignmenetVector += other.direction;
-                centerVector += other.transform.position;
+                //centerVector += other.transform.position;
             }
         }
 
-        Vector3 targetVector = LionSpawner.GetInstance().target.position - transform.position;
-        Vector3 boundaryVector = -transform.position;
+        avoidTargetVector = transform.position - LionSpawner.GetInstance().target.position;
+        avoidTargetVector *= boidSettings.radius / Mathf.Pow(avoidTargetVector.sqrMagnitude / 8, 4);
 
+        float boundaryX = 0;
+        float boundaryY = 0;
+        // upper bound
+        if (LionSpawner.GetTopVibeBound() - transform.position.y < boidSettings.radius)
+        {
+            boundaryY = LionSpawner.GetTopVibeBound() - transform.position.y - boidSettings.radius;
+        }
+        // lower bound
+        else if (-LionSpawner.GetTopVibeBound() - transform.position.y > -boidSettings.radius)
+        {
+            boundaryY = -LionSpawner.GetTopVibeBound() - transform.position.y + boidSettings.radius;
+        }
 
-        Vector3 newDirection = Vector3.zero;
+        // right bound
+        if (LionSpawner.GetRightVibeBound() - transform.position.x < boidSettings.radius)
+        {
+            boundaryX = LionSpawner.GetRightVibeBound() - transform.position.x - boidSettings.radius;
+        }
+        // left bound
+        else if (-LionSpawner.GetRightVibeBound() - transform.position.x > -boidSettings.radius)
+        {
+            boundaryX = -LionSpawner.GetRightVibeBound() - transform.position.x + boidSettings.radius;
+        }
+
+        boundaryVector = new Vector3(boundaryX, boundaryY);
+
+        newDirection = Vector3.zero;
         newDirection += separationVector * boidSettings.separationWeight;
         newDirection += alignmenetVector * boidSettings.alignmentWeight;
         newDirection += centerVector * boidSettings.centerWeight;
 
-        newDirection += targetVector * boidSettings.targetWeight;
+        newDirection += avoidTargetVector * boidSettings.avoidTargetWeight;
         newDirection += boundaryVector * boidSettings.boundaryWeight;
 
         RotateTowards(newDirection);
     }
 
+    public void StartSeeking()
+    {
+        StartCoroutine(SeekTarget());
+    }
+
+    private IEnumerator SeekTarget()
+    {
+        moveState = MoveState.Seeking;
+        spriteRenderer.color = Color.red;
+        Transform target = LionSpawner.GetInstance().target;
+        
+        // move to far enough away
+        while ((transform.position - target.position).magnitude < boidSettings.distUntilAttack)
+        {
+            UpdateDirectionVibing();
+            RotateTowards(avoidTargetVector);
+
+            transform.position += transform.up * boidSettings.speed * Time.deltaTime;
+
+            yield return null;
+        }
+
+        // go for a pass at the target
+        Vector3 acceleration = boidSettings.acceleration * (target.position - transform.position);
+        Vector3 velocity = direction * boidSettings.speed / 2;
+        float minSpeed = 999999;
+        float minDist = (target.position - transform.position).sqrMagnitude;
+        bool staySeeking = true;
+        while (staySeeking)
+        {
+            Vector3 dir = target.position - transform.position;
+            acceleration = boidSettings.acceleration * dir;
+            velocity += acceleration * Time.deltaTime;
+
+            transform.position += velocity * Time.deltaTime;
+            direction = velocity.normalized;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, GetAngle(direction)));
+
+            minDist = Mathf.Min(minDist, dir.sqrMagnitude);
+            // once you've gone far from the center
+            if (dir.sqrMagnitude - minDist > 4)
+            {
+                minSpeed = Mathf.Min(minSpeed, velocity.sqrMagnitude);
+
+                // and speed no longer decreases
+                if (velocity.sqrMagnitude > minSpeed)
+                {
+                    staySeeking = false;
+                }
+            }
+            yield return null;
+        }
+
+
+        moveState = MoveState.Vibing;
+        spriteRenderer.color = Color.white;
+    }
+
     private void RotateTowards(Vector3 targetDirection)
     {
-        direction += (targetDirection.normalized - direction) * boidSettings.rotationSpeed * Time.deltaTime;
+        if (targetDirection.sqrMagnitude < 1)
+            direction += (targetDirection - direction) * boidSettings.rotationSpeed * Time.deltaTime;
+        else
+            direction += (targetDirection.normalized - direction) * boidSettings.rotationSpeed * Time.deltaTime;
+
         direction = direction.normalized;
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, GetAngle(direction)));
     }
 
     private float GetAngle(Vector3 direction)
     {
-        return Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        return Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90; // -90 because the sprite faces up naturally 
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawLine(transform.position, transform.position + direction);
+
+        Gizmos.color = Color.yellow;
+
+        Gizmos.DrawLine(transform.position, transform.position + newDirection);
+
+        Gizmos.color = Color.blue;
+
+        Gizmos.DrawLine(transform.position, transform.position + boundaryVector);
+
+        Gizmos.color = Color.magenta;
+
+        Gizmos.DrawLine(transform.position, transform.position + avoidTargetVector);
     }
 }

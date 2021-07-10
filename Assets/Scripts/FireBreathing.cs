ef22082eacfd48330f369409fb3bb34c91d0f43b
averylg <71529955+averylg@users.noreply.github.com>
2021-07-10 00:08:39 -0400

    Improved Flame Shooting Mechanics
    Slows down flame generation and gives the flame spread when fired

--- Assets/Scripts/FireBreathing.cs
+++ Assets/Scripts/FireBreathing.cs
@@ -4,26 +4,54 @@ using UnityEngine;
 
 public class FireBreathing : MonoBehaviour
 {
-    
+    /**
+     * 
+     */
     public Transform breathStart;
+    public float x, y, z;
     public GameObject flamePrefab;
+    public float maxSpread;
 
     public float flameForce = 10f;
+    public int i = 0;
+    public float rate = 0.1f;
 
+    void Start()
+    {
+        x = breathStart.rotation.eulerAngles.x;
+        y = breathStart.rotation.eulerAngles.y;
+        z = breathStart.rotation.eulerAngles.z;
+    }
+
+    private IEnumerator Breathe()
+    {
+        while (true)
+        {
+            
+            Vector2 dir = breathStart.up + new Vector3(Random.Range(-maxSpread, maxSpread), Random.Range(-maxSpread, maxSpread), 0);
+            GameObject flame = Instantiate(flamePrefab, breathStart.position, breathStart.rotation);
+            Rigidbody2D rb = flame.GetComponent<Rigidbody2D>();
+            rb.AddForce(dir * flameForce, ForceMode2D.Impulse);
+            Debug.Log(i++);
+            yield return new WaitForSeconds(rate);
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Fire1"))
+        if (Input.GetButtonDown("Fire1"))
         {
             ShootFire();
+        }
+        else if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine("Breathe");
         }
     }
 
     void ShootFire()
     {
-        GameObject flame = Instantiate(flamePrefab, breathStart.position, breathStart.rotation);
-        Rigidbody2D rb = flame.GetComponent<Rigidbody2D>();
-        rb.AddForce(breathStart.up * flameForce, ForceMode2D.Impulse);
+        StartCoroutine("Breathe");
     }
 }

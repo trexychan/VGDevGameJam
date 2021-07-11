0ddbcad2fc88e4d98ece9251da1c85a46b2dfb86
necromancer512 <43522920+necromancer512@users.noreply.github.com>
2021-07-11 10:50:53 -0400

    Add level bounds and smooth input control to dragon

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,23 +9,51 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
     public Camera c;
     public Vector2 moveDir, mousePos;
-    
+    public Vector4 levelBounds = new Vector4(4f,8f,-4f,-8f); // top, right, bottom, left
+
+    // Input control
+    public float inputSmoothness = 0.01f;
+    public float inputSmoothingTolerance = 0.01f;
 
     // Update is called once per frame
     void Update()
-    {   
-        moveDir.x = Input.GetAxisRaw("Horizontal");
-        moveDir.y = Input.GetAxisRaw("Vertical");
+    {
+        Vector2 newMoveDir;
+        newMoveDir.x = Input.GetAxisRaw("Horizontal");
+        newMoveDir.y = Input.GetAxisRaw("Vertical");
+        if ((newMoveDir - moveDir).sqrMagnitude > inputSmoothingTolerance)
+        {
+            moveDir = Vector2.Lerp(moveDir, newMoveDir, Time.deltaTime / inputSmoothness);
+        }
+        else
+        {
+            moveDir = newMoveDir;
+        }
 
         mousePos = c.ScreenToWorldPoint(Input.mousePosition);
-
-        
-
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + moveDir * speed * Time.fixedDeltaTime);
+        Vector2 newPosition = rb.position + moveDir * speed * Time.fixedDeltaTime;
+        if (newPosition.y > levelBounds.x)
+        {
+            newPosition.y = levelBounds.x;
+        }
+        if (newPosition.x > levelBounds.y)
+        {
+            newPosition.x = levelBounds.y;
+        }
+        if (newPosition.y < levelBounds.z)
+        {
+            newPosition.y = levelBounds.z;
+        }
+        if (newPosition.x < levelBounds.w)
+        {
+            newPosition.x = levelBounds.w;
+        }
+
+        rb.MovePosition(newPosition);
         Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
